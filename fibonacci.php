5e048044b9d07a803ac434d2bb98500faf63972a
shweta <121001392+shwetaTh@users.noreply.github.com>
2023-04-25 14:04:29 +0545

    Create fibonacci.php

---
+++ fibonacci.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Fibonacci</title>
+  </head>
+  <body>
+  <?php
+    $a = 0;
+    $b = 1;
+    echo $a . ", ";
+    echo $b . ", ";
+    while (($c = $a + $b) <= 100) {
+        echo $c . ", ";
+        $a = $b;
+        $b = $c;
+    }
+    ?>
+</body>
+</html>
